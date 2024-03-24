d12c2f572505bbf1a2ad8b3b1a8929e6cf304dbf
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2024-03-24 21:54:46 +0600

    assignment 3

---
+++ assignment_3/assignment_3/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import include, path
+
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('about-us/', include('about_us.urls')),
+    path('show-items/', include('meal.urls')),
+    
+]
